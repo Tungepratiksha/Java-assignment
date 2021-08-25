5637ed0c8194e873ef64d314a4c7b486268f6379
Tungepratiksha <89512560+Tungepratiksha@users.noreply.github.com>
2021-08-25 21:16:11 +0530

    Add files via upload

---
+++ SumOfTwoNum.java
@@ -0,0 +1,12 @@
+// Write a program in JAVA to print the sum of two numbers.   
+//Sample Output:
+//Print the sum of two numbers :
+//-----------------------------------
+//The sum of 29 and 30 is : 59
+public class SumOfTwoNum {
+ public static void main(String[] args) {
+    System.out.println("Print the sum of two numbers");
+    System.out.println("-------------------------");
+    System.out.println("The sum of 25 and 35 is: "+(25+35));
+ }
+ }
