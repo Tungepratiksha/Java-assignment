3e32b6208bf53edf17a116056f0ed4dd85086491
Tungepratiksha <89512560+Tungepratiksha@users.noreply.github.com>
2021-08-26 16:27:15 +0530

    Add files via upload

---
+++ ComputeExpressions.java
@@ -0,0 +1,15 @@
+// Write a program in JAVA to compute the specified expressions and print the output.   
+//Sample Output:
+//Compute the specified expressions and print the output:
+//------------------------------------------------------------
+//Result of the expression (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5) is : 2.13889
+ 
+public class ComputeExpressions{
+ 
+    public static void main(String[] arg) {
+    	System.out.println("Compute the specified expressions and print the output:");
+    	System.out.println("---------------------------------------------------");
+System.out.println("Result of Expression is (35.5 * 4.5 - 3.5 * 3.5) / (50.5 - 4.5)");
+        System.out.println( (35.5 * 4.5 - 3.5 * 3.5) / (50.5 - 4.5));
+    }
+}
