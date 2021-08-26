66501c42603fdceb89855cb91b44f5d8e5bbea4d
Tungepratiksha <89512560+Tungepratiksha@users.noreply.github.com>
2021-08-26 09:12:33 +0530

    Add files via upload

---
+++ PrimitiveValues.java
@@ -0,0 +1,38 @@
+ 
+//Write a program in JAVA to check whether the primitive values crossing the limits or not.   
+//Check whether the primitive values crossing the limits or not :
+//--------------------------------------------------------------------
+//The Gender is : F
+//Is she married? : 1
+//Number of sons she has : 2
+//Year of her appointment : 2009
+//Salary for a year : 1500000
+//Height is : 79.48
+//GPA is 4.69
+//Salary drawn upto : 12047235
+//Balance till : 995324987
+ 
+class PrimitiveValues { 
+public static void main (String[] args)
+{
+System.out.println("Check whether the primitive values crossing the limits or not :");
+ char gender = 'M'; 
+ boolean isEmployed = false; 
+  int son= 2; 
+ short yearOfAppt = 2013; 
+  int YearlyPackage = 1700000; 
+ double height = 64.8;
+ float gpa = 5.19f;
+ long totalDrawan = 2365235; 
+ long balance = 739205739; 
+System.out.println("The Gender is : " +gender);
+System.out.println("Is she married? :"+isEmployed );
+System.out.println("no of Sons she has:"+son);
+System.out.println("Year of her appointment : "+ yearOfAppt);
+System.out.println("Salary for a year : " + YearlyPackage );
+System.out.println("Height is : " +height);
+System.out.println("GPA is: " +gpa  );
+System.out.println("Salary drawn upto : "+ totalDrawan );
+System.out.println("Balance till : " + balance   );
+}
+}
