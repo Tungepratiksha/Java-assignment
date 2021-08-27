2784b180e37eb27c68e5f398840994cc16163e92
Tungepratiksha <89512560+Tungepratiksha@users.noreply.github.com>
2021-08-27 14:01:04 +0530

    Add files via upload

---
+++ StringCaseConversion.java
@@ -0,0 +1,21 @@
+//Write a JAVA program to replace all the lower-case letters of a given string with the corresponding capital letters.  
+ 
+public class StringCaseConversion {  
+    public static void main(String[] args) {  
+          
+        String str1="pratiksha";  
+        StringBuffer newStr=new StringBuffer(str1);  
+          
+        for(int i = 0; i < str1.length(); i++) {  
+                       if(Character.isLowerCase(str1.charAt(i))) {  
+                           newStr.setCharAt(i, Character.toUpperCase(str1.charAt(i)));  
+            }  
+            
+            else if(Character.isUpperCase(str1.charAt(i))) {  
+                 
+                newStr.setCharAt(i, Character.toLowerCase(str1.charAt(i)));  
+            }  
+        }  
+        System.out.println("String after case conversion : " + newStr);  
+    }  
+}  
